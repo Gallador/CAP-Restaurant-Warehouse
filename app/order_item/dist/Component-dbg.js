1c8965a64dcb403220e3870288824a266e8ff161
Artem Rusetskyi <artem.rusetskyi@gmail.com>
2023-05-19 13:42:36 +0000

    Deploy attempt with error logs

---
+++ app/order_item/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("orderitem.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ app/pizza_product/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("pizzaproduct.Component",{metadata:{manifest:"json"}})});
